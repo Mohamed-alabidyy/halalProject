1c4f849f4eb8c88e14fe0bda611fda7a3a2eab13
mohamed <almasrymyas2005@gmail.com>
2025-05-29 17:37:44 +0200

    ta3deil ethnyn

--- halal.java
+++ halal.java
@@ -1,6 +1,7 @@
 public class halal{
 	
 	public static void main (String[] args) {
-		System.out.print("halal one");
+		System.out.println("halal one");
+		System.out.println("halal one again");
 	}
 }
--- halal2.java
+++ halal2.java
@@ -1,6 +1,6 @@
 public class halal2{
 	
 	public static void main (String[] args) {
-		System.out.print("halal two");
+		System.out.println("halal two");
 	}
 }
